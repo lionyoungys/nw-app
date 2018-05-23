2a930af37e66651d13f60c4a61ba336ff167a92c
EdwinYoung <go.xlv@qq.com>
2018-05-23 15:43:37 +0800

    界面搭建

--- src/login.js
+++ src/login.js
@@ -20,11 +20,18 @@ class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {index:2, version:'3.0.2', log:'dfdfddfdfdf'};
+        this.toggleStep = this.toggleStep.bind(this);
         this.step = [
-            <Launch/>,
+            <Launch toggleStep={this.toggleStep}/>,
             <Download version={this.state.version} log={this.state.log}/>,
-            <Login/>
+            <Login toggleStep={this.toggleStep}/>,
+            <Passwd toggleStep={this.toggleStep}/>
         ];
+        
+    }
+
+    toggleStep(e) {
+        this.setState({index:e.target.dataset.step});
     }
 
     render() {
@@ -89,6 +96,11 @@ class Download extends Component {
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {remember:false,merchant:'',name:'',passwd:''}
+        this.remember = this.remember.bind(this);
+    }
+    remember() {
+        this.setState({remember:!this.state.remember});
     }
 
     render() {
@@ -96,11 +108,78 @@ class Login extends Component {
             <div className='login'>
                 <div className='login-edition'><div>速洗达洗衣管理系统</div><div>（商家版）</div></div>
                 <div className='login-box'>
-                    <div><label htmlFor='name'>用户名：</label><input type='text' id='name' className=''/></div>
-                    <div style={{padding:'21px 0 25px'}}><label htmlFor='passwd'>密&emsp;码：</label><input type='password' id='passwd'/></div>
-                    <div></div>
-                    <div style={{padding:'25px 0 24px 9px'}}></div>
-                    <div style={{paddingLeft:'9px'}}></div>
+                    <div className='login-row'>
+                        <label htmlFor='merchant'>商户号：</label>
+                        <input type='text' id='merchant' className='login-input' value={this.state.merchant} onChange={e => this.setState({merchant:e.target.value})}/>
+                    </div>
+                    <div className='login-row'>
+                        <label htmlFor='name'>用户名：</label>
+                        <input type='text' id='name' className='login-input' value={this.state.name} onChange={e => this.setState({name:e.target.value})}/>
+                    </div>
+                    <div className='login-row'>
+                        <label htmlFor='passwd'>密&emsp;码：</label>
+                        <input type='password' id='passwd' className='login-input' value={this.state.passwd} onChange={e => this.setState({passwd:e.target.value})}/>
+                    </div>
+                    <div className='login-passwd-handle'>
+                        <span className={this.state.remember ? 'checked' : null} onClick={this.remember}>记住密码</span>
+                        <span onClick={this.props.toggleStep} data-step='3'>找回密码</span>
+                    </div>
+                    <div style={{paddingLeft:'10px'}}><button type='button'>登陆</button></div>
+                </div>
+            </div>
+        );
+    }
+}
+//找回密码
+class Passwd extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {passwd:'',passwd2:'',phone:'',sms:'',smsCounter:'获取验证码'}
+        this.counter = 60;
+        this.timeId = null;
+        this.sms = this.sms.bind(this);
+    }
+    sms() {
+        if ('获取验证码' !== this.state.smsCounter) return;
+        this.setState({smsCounter:'60s'});
+        this.timeId = setInterval(() => {
+            --this.counter;
+            if (0 === this.counter) {
+                clearInterval(this.timeId);
+                this.timeId = null;
+                this.setState({smsCounter:'获取验证码'});
+            } else {
+                this.setState({smsCounter:this.counter + 's'});
+            }
+        }, 1000);
+    }
+    componentWillUnmount() {null !== this.timeId && clearInterval(this.timeId)}
+
+    render() {
+        return (
+            <div className='passwd'>
+                <div className='login-edition'><div>速洗达洗衣管理系统</div><div>（商家版）</div></div>
+                <div className='passwd-box'>
+                    <div>
+                        <label htmlFor='new_passwd'>新密码：</label>
+                        <input type='password' id='new_passwd' className='login-input' value={this.state.passwd} onChange={e => this.setState({passwd:e.target.value})}/>
+                        <div style={{marginLeft:'242px',fontSize:'10px',color:'#ff0000'}}>密码为6~18位字母、数字组合</div>
+                    </div>
+                    <div>
+                        <label htmlFor='new_passwd2'>确认密码：</label>
+                        <input type='password' id='new_passwd2' className='login-input' value={this.state.passwd2} onChange={e => this.setState({passwd2:e.target.value})}/>
+                    </div>
+                    <div style={{margin:'12px 0'}}>
+                        <label htmlFor='phone'>手机号：</label>
+                        <input type='text' id='phone' className='login-input' value={this.state.phone} onChange={e => this.setState({phone:e.target.value})}/>
+                    </div>
+                    <div>
+                        <label htmlFor='sms'>验证码：</label>
+                        <input type='text' style={{width:'102px'}} id='sms' className='login-input' value={this.state.sms} onChange={e => this.setState({sms:e.target.value})}/>
+                        <span className='sms' onClick={this.sms}>{this.state.smsCounter}</span>
+                    </div>
+                    <div style={{padding:'20px 0 16px 178px'}}><button type='button'>确认</button></div>
+                    <div><span onClick={this.props.toggleStep} data-step='2' className='back-to-login'>返回登陆</span></div>
                 </div>
             </div>
         );
