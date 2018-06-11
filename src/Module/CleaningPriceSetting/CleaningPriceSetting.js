5801d8c7f4a1c1fbc4dfe0ff7ba3be826b7786dd
EdwinYoung <go.xlv@qq.com>
2018-06-11 13:33:50 +0800

    界面搭建

--- src/Module/CleaningPriceSetting/CleaningPriceSetting.js
+++ src/Module/CleaningPriceSetting/CleaningPriceSetting.js
@@ -151,20 +151,20 @@ export default class extends Component {
                     <Window title='新增洗护价格' onClose={() => this.setState({show:false})} width="510" height="312">
                         <div className="addnewprice">
                             <div className="addnewprice-div">
-                                <div><span>衣物处理：</span><Select option={['干洗','水洗','烘干']} selected='干洗' onChange={value => console.log(value)} /></div>
-                                <div><span>衣物类别：</span><Select option={['上衣','裤装','小件']} selected='上衣' onChange={value => console.log(value)}/></div>
+                                <div className="addnewprice-div-select"><span>衣物处理：</span><Select option={['干洗','水洗','烘干']} selected='干洗' onChange={value => console.log(value)} /></div>
+                                <div className="addnewprice-div-select"><span>衣物类别：</span><Select option={['上衣','裤装','小件']} selected='上衣' onChange={value => console.log(value)}/></div>
                             </div>
                             <div className="addnewprice-div">
-                                <div><span>名称：</span><input  type="text"/></div>
-                                <div><span>档次：</span><Select option={['普通','高档']} selected='普通' onChange={value => console.log(value)}/></div>
+                                <div className="addnewprice-div-nor"><span>名称：</span><input  type="text"/></div>
+                                <div className="addnewprice-div-select"><span>档次：</span><Select option={['普通','高档']} selected='普通' onChange={value => console.log(value)}/></div>
                             </div>
                             <div className="addnewprice-div">
-                                <div><span>交活天数：</span><input  type="text"/></div>
-                                <div><span>格架系数：</span><input  type="text"/></div>
+                                <div className="addnewprice-div-nor"><span>交活天数：</span><input  type="text"/></div>
+                                <div className="addnewprice-div-nor"><span>格架系数：</span><input  type="text"/></div>
                             </div>
                             <div className="addnewprice-div">
-                                <div><span>价格：</span><input  type="text"/></div>
-                                <div><span>折扣下限：</span><input  type="text" className="addnewprice-dicount"/> %</div>
+                                <div className="addnewprice-div-nor"><span>价格：</span><input  type="text"/></div>
+                                <div className="addnewprice-div-nor"><span>折扣下限：</span><input  type="text" className="addnewprice-dicount"/> %</div>
                             </div>
                             <div className="addnewprice-money">
                                 <input type="checkbox" />价格可调
