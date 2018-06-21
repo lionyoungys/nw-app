1e92c7961828bcac20bb9a6e826f7f9bd2d422c9
EdwinYoung <go.xlv@qq.com>
2018-06-21 18:19:09 +0800

    界面搭建

--- src/Module/Charts/Amount.js
+++ src/Module/Charts/Amount.js
@@ -3,11 +3,28 @@
  * @author Edwin Young
  */
 import React from 'react';
+import Highcharts from 'highcharts';
 
 export default class extends React.Component {
     constructor(props) {super(props)}
 
+    componentDidMount() {
+        Highcharts.chart(this.container, {
+            title: {text: '月度营业额分析'},
+            credits: {enabled: false},
+            yAxis: {title: {text: ''}},
+            xAxis: {categories: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']},
+            tooltip: {pointFormat: '<b>{point.y}</b>'},
+            series: [{
+                type: 'column',
+                colorByPoint: true,
+                data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+                showInLegend: false
+            }]
+        });
+    }
+
     render() {
-        return (<div>月度营业额分析</div>);
+        return (<div ref={container => this.container = container}></div>);
     }
 }
--- src/Module/Charts/Cost.js
+++ src/Module/Charts/Cost.js
@@ -3,6 +3,7 @@
  * @author Edwin Young
  */
 import React from 'react';
+import Highcharts from 'highcharts';
 
 export default class extends React.Component {
     constructor(props) {super(props)}
