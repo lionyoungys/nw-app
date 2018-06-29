63b8c5817e9a17e54a335c24bc7c87993f2531f5
EdwinYoung <go.xlv@qq.com>
2018-06-29 14:01:27 +0800

    界面修改

--- src/Module/Demo.js
+++ src/Module/Demo.js
@@ -91,8 +91,8 @@ export default class extends React.Component {
                 <button type='button' className='e-btn middle' readOnly>取消</button>
                 <button type='button' className='e-btn large' readOnly>取消</button>
                 <br/>
-                <button type='button' className='e-btn' onClick={() => EventApi.print('order', {test:1,param:2,cd:'erererr', word:'你好啊！'})}>订单打印</button>
-                <button type='button' className='e-btn middle' onClick={() => EventApi.print('card', 'id=50&cid=60&word=你好啊！')}>会员卡打印</button>
+                <button type='button' className='e-btn' onClick={() => EventApi.print('order', {test:1,param:2,cd:'erererr', word:'你好啊！', take:1})}>订单打印</button>
+                <button type='button' className='e-btn middle' onClick={() => EventApi.print('card', 'id=50&cid=60&word=你好啊！&take=1')}>会员卡打印</button>
                 <br/>
                 <button type='button' className='e-btn' onClick={this.ask}>询问弹框</button>
                 <button type='button' className='e-btn' onClick={this.error}>错误弹框</button>
