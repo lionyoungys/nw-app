fa0c52b53de1c26e909e252c5e823d81b91e0f3c
EdwinYoung <go.xlv@qq.com>
2018-10-13 18:01:53 +0800

    界面搭建

--- src/Module/Clothes/Category.js
+++ src/Module/Clothes/Category.js
@@ -18,15 +18,15 @@ export default class extends Component {
     render() {
         if ('undefined' === typeof this.props.data || !(this.props.data instanceof Array)) return null;
         let html = this.props.data.map((obj, index) =>
-            <i key={obj.id} data-index={index} onClick={this.handleClick}>{obj.name}</i>
+            <i className='e-option' key={obj.id} data-index={index} onClick={this.handleClick}>{obj.name}</i>
         );
         return (
-            <Dish title='编辑衣物信息' height='454' width='648' onClose={this.props.onClose}>
-                <div className='clothes-editor-top'>
+            <Dish title='编辑衣物信息' onClose={this.props.onClose}>
+                <div className='clothes-cate-top'>
                     <span>衣物类别</span>
-                    <button type='button' className='e-btn' onClick={this.props.onClick}>临时衣物</button>
+                    <button type='button' className='e-btn larger' onClick={this.props.onClick}>临时衣物</button>
                 </div>
-                <div className='clothes-editor-body'>{html}</div>
+                <div className='clothes-cate-body'>{html}</div>
             </Dish>
         );
     }
