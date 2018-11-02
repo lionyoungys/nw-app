987e7bd877fa05d453910cb24fe8dedcdd273575
EdwinYoung <go.xlv@qq.com>
2018-11-02 15:31:48 +0800

    界面搭建

--- src/Module/TableDemo.js
+++ src/Module/TableDemo.js
@@ -15,17 +15,42 @@ export default class extends React.Component {
     render() {
         return (
             <Window title='表格demo' onClose={this.props.closeView} padding={true}>
-                <Table>
-                    <thead><tr><th>head1</th><th>head2</th><th>head3</th><th>head4</th></tr></thead>
-                    <tbody>
-                        <tr><td>头1</td><td>头2</td><td>头3</td><td>头4</td></tr>
-                        <tr><td>头1</td><td>头2</td><td>头3</td><td>头4</td></tr>
-                        <tr><td>头1</td><td>头2</td><td>头3</td><td>头4</td></tr>
-                        <tr><td>头1</td><td>头2</td><td>头3</td><td>头4</td></tr>
-                        <tr><td>头1</td><td>头2</td><td>头3</td><td>头4</td></tr>
-                        <tr><td>头1</td><td>头2</td><td>头3</td><td>头4</td></tr>
-                        <tr><td>头1</td><td>头2</td><td>头3</td><td>头4</td></tr>
-                        <tr><td>头1</td><td>头2</td><td>头3</td><td>头4</td></tr>
+                <Table style={{height:'200px'}}>
+                    <thead>
+                        <tr><th>衣物编码</th><th>搁架号</th><th>衣物名称</th><th>颜色</th></tr>
+                    </thead>
+                    <tbody style={{textAlign:'center'}}>
+                        <tr><td>050900000001</td><td>A0775-01</td><td>西装上衣</td><td>灰色</td></tr>
+                        <tr><td>050900000002</td><td>A0827-01</td><td>西装上衣</td><td>灰色</td></tr>
+                        <tr><td>100800000003</td><td>A0721-01</td><td>西装上衣</td><td>黑色</td></tr>
+                        <tr><td>100800000003</td><td>A0721-02</td><td>羊毛裤</td><td>紫格</td></tr>
+                        <tr><td>100800000003</td><td>A0721-03</td><td>百褶裙（短）</td><td>米黄色</td></tr>
+                        <tr><td>100800000004</td><td>A0666-01</td><td>西装上衣</td><td>黑色</td></tr>
+                        <tr><td>100800000004</td><td>A0666-02</td><td>羊毛裤</td><td>紫拼</td></tr>
+                        <tr><td>100800000004</td><td>A0666-03</td><td>羊毛衫</td><td>米黄色</td></tr>
+                        <tr><td>112700000005</td><td>A0147-01</td><td>休闲上衣</td><td>淡绿</td></tr>
+                        <tr><td>122500000006</td><td>A0647-01</td><td>西装上衣</td><td>黑色</td></tr>
+                        <tr><td>122500000006</td><td>A0647-02</td><td>羊毛裤</td><td>紫格</td></tr>
+                        <tr><td>020500000013</td><td>A0282-01</td><td>羽绒中长</td><td>白色</td></tr>
+                        <tr><td>020500000013</td><td>A0282-02</td><td>羽绒长</td><td>白色</td></tr>
+                        <tr><td>020500000013</td><td>A0282-03</td><td>羽绒中长</td><td>黑色</td></tr>
+                        <tr><td>020500000013</td><td>A0283-04</td><td>羽绒中长</td><td>黑色</td></tr>
+                        <tr><td>050900000001</td><td>A0775-01</td><td>西装上衣</td><td>灰色</td></tr>
+                        <tr><td>050900000002</td><td>A0827-01</td><td>西装上衣</td><td>灰色</td></tr>
+                        <tr><td>100800000003</td><td>A0721-01</td><td>西装上衣</td><td>黑色</td></tr>
+                        <tr><td>100800000003</td><td>A0721-02</td><td>羊毛裤</td><td>紫格</td></tr>
+                        <tr><td>100800000003</td><td>A0721-03</td><td>百褶裙（短）</td><td>米黄色</td></tr>
+                        <tr><td>100800000004</td><td>A0666-01</td><td>西装上衣</td><td>黑色</td></tr>
+                        <tr><td>100800000004</td><td>A0666-02</td><td>羊毛裤</td><td>紫拼</td></tr>
+                        <tr><td>100800000004</td><td>A0666-03</td><td>羊毛衫</td><td>米黄色</td></tr>
+                        <tr><td>050900000001</td><td>A0775-01</td><td>西装上衣</td><td>灰色</td></tr>
+                        <tr><td>050900000002</td><td>A0827-01</td><td>西装上衣</td><td>灰色</td></tr>
+                        <tr><td>100800000003</td><td>A0721-01</td><td>西装上衣</td><td>黑色</td></tr>
+                        <tr><td>100800000003</td><td>A0721-02</td><td>羊毛裤</td><td>紫格</td></tr>
+                        <tr><td>100800000003</td><td>A0721-03</td><td>百褶裙（短）</td><td>米黄色</td></tr>
+                        <tr><td>100800000004</td><td>A0666-01</td><td>西装上衣</td><td>黑色</td></tr>
+                        <tr><td>100800000004</td><td>A0666-02</td><td>羊毛裤</td><td>紫拼</td></tr>
+                        <tr><td>100800000004</td><td>A0666-03</td><td>羊毛衫</td><td>米黄色</td></tr>
                     </tbody>
                 </Table>   
             </Window>
--- src/UI/Table.js
+++ src/UI/Table.js
@@ -1,19 +1,76 @@
 /**
  * table组件
  * @author Edwin Young
- * @desc table
+ * @desc table    className:追加css类名;style:追加style样式;tableClassName:组件内部table追加css类名;tableStyle:组件内部table追加style样式;
  */
 
 import React, {Component} from 'react';
 export default class extends Component {
     constructor(props) {
         super(props);
+        this.state = {top:0, lineHeight:0, children:[]};
+        this.handleWindowResize = this.handleWindowResize.bind(this);
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    componentDidMount() {
+        this.handleWindowResize();
+        window.addEventListener('resize', this.handleWindowResize);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowResize);
+    }
+
+    handleWindowResize() {
+        let nodes = this.refs.table.childNodes
+        ,   len = nodes.length
+        ,   node;
+        if (len > 0) {
+            for (let i = 0;i < len;++i) {
+                if ('THEAD' === nodes[i].tagName || 'TBODY' === nodes[i].tagName) {
+                    node = nodes[i].childNodes[0];
+                    break;
+                }
+            }
+            if ('object' === typeof node && 'TR' === node.tagName) {
+                let tds = node.childNodes
+                ,   tdLen = tds.length
+                ,   children = [];
+                for (let i = 0;i < tdLen;++i) {
+                    children.push({
+                        key: ('td_' + i), 
+                        html: tds[i].innerHTML, 
+                        style:{width:(tds[i].scrollWidth + 'px'), height:(tds[i].scrollHeight + 'px')}
+                    });
+                }
+                this.setState({lineHeight:(node.offsetHeight + 'px'), children:children});
+            }
+        }
+    }
+
+    handleScroll(e) {
+        e.persist();
+        this.setState({top:(e.target.scrollTop + 'px')});
     }
 
     render() {
+        let html = this.state.children.map(obj => <div style={obj.style} key={obj.key}>{obj.html}</div>)
+        ,   theme = this.props.theme
+        ,   className = 'ui-table';
+        if ('string' === typeof theme) {
+            if ('none' !== theme) {
+                className += (' ui-table-' + theme);
+            }
+        } else {
+            className += ' ui-table-default';
+        }
+        if ('string' === typeof this.props.className) {
+            className += (' ' + this.props.className);
+        }
         return (
-            <div className='ui-table'>
-                <table className={this.props.className} style={this.props.style}>
+            <div className={className} style={this.props.style} onScroll={this.handleScroll}>
+                <section style={{lineHeight:this.state.lineHeight, top:this.state.top}}>{html}</section>
+                <table className={this.props.tableClassName} style={this.props.tableStyle} ref='table'>
                     {this.props.children}
                 </table>
             </div>
